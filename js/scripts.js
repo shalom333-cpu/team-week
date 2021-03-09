fe95eda170962bab0cae9e694e5df6f0e5618f9c
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-03-09 15:24:58 +0300

    toggle betwen the hotel names and descriptions

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$("#hotel-1").click(function() {
+    $(".description").toggle("slow");
+});
+
+$("#hotel-2").click(function() {
+    $(".description-2").toggle("slow");
+});
+
+$("#hotel-3").click(function() {
+    $(".description-3").toggle("slow");
+});
+
+$("#hotel-4").click(function() {
+    $(".description-4").toggle("slow");
+});
+
+$("#hotel-5").click(function() {
+    $(".description-5").toggle("slow");
+});
+
+$("#hotel-6").click(function() {
+    $(".description-6").toggle("slow");
+});
+
+$("#hotel-7").click(function() {
+    $(".description-7").toggle("slow");
+});
+
+$("#hotel-8").click(function() {
+    $(".description-8").toggle("slow");
+});
+
+$("#hotel-9").click(function() {
+    $(".description-9").toggle("slow");
+});
+
+$("#hotel-10").click(function() {
+    $(".description-10").toggle("slow");
+});
