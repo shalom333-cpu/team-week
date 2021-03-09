bfca7c4b8a6ffc46d7664d16bcc9adb1c59f8579
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-03-09 19:38:38 +0300

    booking forms were added

--- js/scripts.js
+++ js/scripts.js
@@ -37,3 +37,43 @@ $("#hotel-9").click(function() {
 $("#hotel-10").click(function() {
     $(".description-10").toggle("slow");
 });
+
+
+$("button#book-now").click(function() {
+    $(".description").hide();
+    $(".description-2").hide();
+    $(".description-3").hide();
+    $(".description-4").hide();
+    $(".description-5").hide();
+    $(".description-6").hide();
+    $(".description-7").hide();
+    $(".description-8").hide();
+    $(".description-9").hide();
+    $(".description-10").hide();
+    $(".form-book").show();
+    
+});
+
+
+
+const forms = document.getElementsByClassName("book-form"); //gets a list of elements with the class name
+for (let i = 0; i < forms.length; i++) { //for loop - loops through the list
+    const form = forms.item(i); // gets form from the list
+    form.addEventListener("submit",(e) => {
+        e.preventDefault(); 
+        const formData = new FormData(e.target); 
+        var checkIn = new Date(formData.get("check_in")); 
+        var checkOut = new Date(formData.get("check_out")); 
+        var price = formData.get("price");
+
+        var userName = $("input#name").val();
+        
+        var days = (checkOut.getTime() - checkIn.getTime()) / (24 * 3600 * 1000);
+
+        alert("Hello " + userName + "Your Booking reservation is confirmed. The total amount ypu will pay is: " + days * price);
+
+    });
+}
+$("button#full").click(function() {
+    $(".form-book").hide();
+});
