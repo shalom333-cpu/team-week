8ca30b2fbc35b2ee7aaac2b110361b7e4ae4807b
Alvin Awuor <alvinawuor8@gmail.com>
2021-03-11 09:38:04 +0300

    Reset form after submitting.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-$("#contact").submit(function (e) {
-    e.preventDefault();
-})
+$(document).ready(function() {
+    $("#contact").submit(function(e) {
+        e.preventDefault();
+
+        function reset() {
+            $("#f-name").val("");
+            $("#l-name").val("");
+            $("#email").val("");
+            $("#message").val("");
+        }
+
+        var name = $("#f-name").val();
+        alert("Thank you " + name + "\nWe have received your response. 😁");
+
+        reset();
+    });
+});
